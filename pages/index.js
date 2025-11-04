0e21110ab0e9840ad46b2f852c3dce8210659613
Ronald Wahome <wahomeron@gmail.com>
2025-11-04 04:53:25 +0000

    Add listings, about page, and feature updates1

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
               lineHeight: "1.6",
             }}
           >
-            Data-driven. Curated. Verified. Homes.
+            Curated. Verified. Data-driven.
           </p>
 
           <p
