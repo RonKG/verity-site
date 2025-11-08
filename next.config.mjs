ddbdbc8099ec394f66f3bc9abe3288aaa6ceaee9
Ronald Wahome <wahomeron@gmail.com>
2025-11-08 00:24:00 +0000

    fix(config): add missing image hostnames to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
       { protocol: 'https', hostname: 'plus.unsplash.com' },
       { protocol: 'https', hostname: 'www.elitehomeskenya.com' },
       { protocol: 'https', hostname: 'img.jamesedition.com' },
-      { protocol: 'https', hostname: 'www.flamaproperties.com' }
+      { protocol: 'https', hostname: 'www.flamaproperties.com' },
+      { protocol: 'https', hostname: 'kitchenmastersllc.com' },
+      { protocol: 'https', hostname: 'www.decorilla.com' },
+      { protocol: 'https', hostname: 'www.peerspace.com' }
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
